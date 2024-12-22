8b7c0b28fe65dbb97a867261db6967ab0c6c6178
horiidonuts <eray.tuna1221@gmail.com>
2024-12-22 23:22:12 +0300

    8 directional movement completed
    Added 8 directional movement and implemented animations for walking

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,49 +1,137 @@
 using System;
+using System.Drawing;
 using System.Transactions;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    Animator animator;
     [SerializeField] float moveSpeed = 1f;
-    Vector3 rawInput;
+
+    int wLeftHash;
+    int wRightHash;
+    int wUpHash;
+    int wDownHash;
+
+    PlayerInput playerInput;
+    InputAction moveAction;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+        wUpHash = Animator.StringToHash("wUp");
+        wDownHash = Animator.StringToHash("wDown");
+        wLeftHash = Animator.StringToHash("wLeft");
+        wRightHash = Animator.StringToHash("wRight");    
+    }
 
     void Update()
     {
-        Move();
+        HandleMove();
     }
 
-    void Move()
+    void HandleMove()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
-    }
-
-    void OnMove(InputValue value)
-    {
-        Vector3 input = value.Get<Vector3>();
-
-        float x_input = input.x;
-        float z_input = input.z;
-
-        if (input.z > Mathf.Epsilon || input.z < -Mathf.Epsilon)
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(-direction.x + -direction.y, 0, -direction.y + direction.x) * moveSpeed * Time.deltaTime;
+        
+        //  full up
+        if (new Vector3(-direction.x + -direction.y, 0, -direction.y + direction.x) == new Vector3(-1, 0, -1))
         {
-            rawInput = new Vector3(z_input * -0.5f, input.y, z_input * -0.5f);
-        }
-        else if(input.x > Mathf.Epsilon || input.x < -Mathf.Epsilon)
-        {
-            rawInput = new Vector3(x_input * -0.5f, input.y, x_input * 0.5f);
+            animator.SetBool(wUpHash, true);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wLeftHash,false);
+            animator.SetBool(wRightHash,false);
+            
         }
 
+        //  full left
+        else if (new Vector3(-direction.x + -direction.y, 0, -direction.y + direction.x) == new Vector3(1, 0, -1))
+        {
+            animator.SetBool(wLeftHash, true);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wRightHash,false);
+        }
+
+        //  upleft
+        else if (direction.x < 0 && direction.y > 0)
+        {
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wLeftHash,true);
+            animator.SetBool(wRightHash,false);
+        }
+
+        // full right
+        else if (new Vector3(-direction.x + -direction.y, 0, -direction.y + direction.x) == new Vector3(-1, 0, 1))
+        {
+            animator.SetBool(wRightHash, true);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wLeftHash,false);
+        }
+
+        //  full down
+        else if (new Vector3(-direction.x + -direction.y, 0, -direction.y + direction.x) == new Vector3(1, 0, 1))
+        {
+            animator.SetBool(wDownHash, true);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wRightHash, false);
+            animator.SetBool(wLeftHash,false);
+        }
+
+        //  upright
+        else if (direction.x > 0 && direction.y > 0)
+        {
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wLeftHash,false);
+            animator.SetBool(wRightHash,true);
+        }
+
+        // downright
+        else if (direction.x > 0 && direction.y < 0)
+        {
+            animator.SetBool(wDownHash, true);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wRightHash, false);
+            animator.SetBool(wLeftHash,false);
+        }
+
+        //  downleft
+        else if (direction.x < 0 && direction.y < 0)
+        {
+            animator.SetBool(wDownHash, true);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wLeftHash, false);
+            animator.SetBool(wRightHash,false);
+            
+        }
+
+        //  standing still
         else 
         {
-            rawInput = new Vector3(0, 0, 0);
+            animator.SetBool(wUpHash, false);
+            animator.SetBool(wDownHash,false);
+            animator.SetBool(wLeftHash,false);
+            animator.SetBool(wRightHash,false);
         }
 
-        
+        Debug.Log(direction);
 
-        Debug.Log(rawInput);
     }
 
+    void MoveAnim()
+    {
+        bool wUp = animator.GetBool(wUpHash);
+        bool wDown = animator.GetBool(wDownHash);
+        bool wLeft = animator.GetBool(wLeftHash);
+        bool wRight = animator.GetBool(wRightHash);
+    }
 }
