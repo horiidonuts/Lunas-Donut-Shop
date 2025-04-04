93476e12dec065d1e04b37a4be3b9b3f6f075ccd
horiidonuts <eray.tuna1221@gmail.com>
2025-04-05 02:26:04 +0300

    -

--- Assets/Scripts/Kitchen Scripts/UpdateMeter.cs
+++ Assets/Scripts/Kitchen Scripts/UpdateMeter.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
 using DG.Tweening;
-using DG.Tweening.Core;
-using TMPro;
+using TMPro;     
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
