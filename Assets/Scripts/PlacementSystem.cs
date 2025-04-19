ff28c2daefa5a646d175021e1744b1f2e3f3e7aa
Muhammedeminozkan <147250340+Muhammedeminozkan@users.noreply.github.com>
2025-04-19 17:26:24 +0300

    added grid 1

---
+++ Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private InputManager InputManager;
+    [SerializeField]
+    private Grid grid;
+
+    private void Update()
+    {
+        Vector3 mousePosition = InputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
